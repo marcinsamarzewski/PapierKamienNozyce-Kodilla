bb4285cf43a2c7391b545bc45f15f0f8aac64ca9
Marcin Samarzewski <m.samarzewski1990@gmail.com>
2021-06-24 19:01:55 +0200

    add js code -combine number with text

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nozyce';
+}
+printMessage('Mój ruch: ' + computerMove);
+computerMove = 0;
