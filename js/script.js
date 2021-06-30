ff49f11c9d47ae5859bde8c5388e86f0da71b11f
Marcin Samarzewski <m.samarzewski1990@gmail.com>
2021-06-30 18:50:42 +0200

    cnage let to const

--- js/script.js
+++ js/script.js
@@ -56,11 +56,11 @@ function buttonClicked(argButtonName ) {
     }
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
   }
-    playerMove = argButtonName;
+    const playerMove = argButtonName;
     console.log('ruch gracza to: ' + playerMove);
-    randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('wylosowana liczba to: ' + randomNumber);
-    computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
     displayResult(playerMove, computerMove);
 
